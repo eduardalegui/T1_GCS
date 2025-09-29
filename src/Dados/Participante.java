eac80e44969bf3e7ceb7476c1ec1d16562c4b420
Eduardo Hoffmann Araújo <eduardo.hoffmann1010@gmail.com>
2025-09-29 20:21:46 -0300

    Update Participante.java

--- src/Dados/Participante.java
+++ src/Dados/Participante.java
@@ -7,3 +7,61 @@ public class Participante {
     private int idade;
     
 }
+
+//package Dados;
+
+public class Participante {
+    private Ingresso ingresso;
+    private String nome;
+    private String cpf;
+    private int idade;
+    
+    public Participante(Ingresso ingresso, String nome, String cpf, int idade) {
+        this.ingresso = ingresso;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public Ingresso getIngresso() {
+        return ingresso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIngresso(Ingresso ingresso) {
+        this.ingresso = ingresso;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Participante{" +
+               "ingressoId=" + (ingresso != null ? ingresso.getIdIngresso() : "N/A") +
+               ", nome=\'" + nome + "\'" +
+               ", cpf=\'" + cpf + "\'" +
+               ", idade=" + idade +
+               "}";
+    }
+}
