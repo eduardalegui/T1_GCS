6a6a198b0bf5999e69ef26524cf43621d0f99ad7
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-09-29 11:39:16 -0300

    Criação da classe Main, chamada do método executar e correção de comentário no App.

--- src/Aplicacao/App.java
+++ src/Aplicacao/App.java
@@ -1,5 +1,5 @@
-// Imports
 package Aplicacao;
+// Importações;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Aplicacao.App; // Importa a classe App que se encontra na pasta Aplicacao; 
+
+public class Main {
+    public static void main(String[] args) {
+        App app = new App(); 
+        app.executar(); // Chama o metodo executar a partir do app;
+    }
+}
