e2a9cbcc3607ce26eebd4c7b064d2edb903d178e
matheusbtguerra <matheus05guerra@gmail.com>
2025-10-01 00:02:04 +0000

    refactor: Atualiza e implementa atributos e métodos da classe ingresso

--- src/Dados/Ingresso.java
+++ src/Dados/Ingresso.java
@@ -1,55 +1,63 @@
 package Dados;
 
 public class Ingresso {
-    private Evento evento;
-    private boolean especial = true;
-    private String codigo;
-    private int idIngresso;
-    
-    public Ingresso(Evento evento, boolean especial, String codigo, int idIngresso) {
-        this.evento = evento;
-        this.especial = especial;
-        this.codigo = codigo;
-        this.idIngresso = idIngresso;
-    }
-    public Evento getEvento() {
-        return evento;
+    private String codigo; // Ex: "121-001" ou "121-076E" precisa ser sequencial e ter um codigo do evento, além de um sufixo E para especial
+    private int sequencial;
+    private int tipo; // 1=normal, 2=especial
+    private boolean presente; //true=presente, false=ausente
+    private int codigoEvento; //Não necessita de uma instância de Evento, pois o codigo do evento é unico e pode ser usado para buscar o evento em uma lista
+
+    public Ingresso(int sequencial, int tipo, int codigoEvento) {
+        this.sequencial = sequencial; //para manter uma sequencia dentro de um arraylist pode ser passado como parametro (arraylist.size()+1)
+        this.tipo = tipo;
+        this.presente = false;
+        this.codigoEvento = codigoEvento;
+        this.codigo = gerarCodigo();
     }
 
-    public boolean isEspecial() {
-        return especial;
+    //apenas getters necessarios e sem setters para manter a integridade do ingresso
+    public int getTipo() {
+        return tipo;
+    }
+
+    public boolean isPresente() {
+        return presente;
+    }
+
+    public int getCodigoEvento() {
+        return codigoEvento;
     }
 
     public String getCodigo() {
         return codigo;
     }
 
-    public int getIdIngresso() {
-        return idIngresso;
-    }
-    public void setEvento(Evento evento) {
-        this.evento = evento;
-    }
-
-    public void setEspecial(boolean especial) {
-        this.especial = especial;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public void setIdIngresso(int idIngresso) {
-        this.idIngresso = idIngresso;
-    }
-
     @Override
     public String toString() {
-        return "Ingresso{" +
-               "eventoId=" + (evento != null ? evento.getIdEvento() : "N/A") +
-               ", especial=" + especial +
-               ", codigo=\'" + codigo + "\'" +
-               ", idIngresso=" + idIngresso +
-               '\n' + "}";
+        String tipoStr = (tipo == 1) ? "Normal" : (tipo == 2) ? "Especial" : "Desconhecido";
+        String presencaStr = presente ? "Presente" : "Ausente";
+        return String.format(
+                "===========================\n" +
+                        "Ingresso:\n" +
+                        "  Código: %s\n" +
+                        "  Sequencial: %d\n" +
+                        "  Tipo: %s\n" +
+                        "  Presença: %s\n" +
+                        "  Código do Evento: %d" +
+                        "===========================\n" +
+                        codigo, sequencial, tipoStr, presencaStr, codigoEvento
+        );
     }
+
+    private String gerarCodigo(){
+        if(this.tipo == 1){
+            return String.format("%d-%d", this.codigoEvento, this.sequencial);
+        } else {
+            return String.format("%d-%dE", this.codigoEvento, this.sequencial);
+        }
+    }
+
+    public void marcarPresenca(){
+        this.presente = true;
+    };
 }
