ebe53545e8c3b27300e5cb49df8485648a5172a2
Eduardo Hoffmann Araújo <eduardo.hoffmann1010@gmail.com>
2025-09-30 23:43:57 -0300

    Update Evento.java

--- src/Dados/Evento.java
+++ src/Dados/Evento.java
@@ -6,4 +6,65 @@ public class Evento {
     private int valor;
     private int qntVagas;
     private int idEvento;
+
+    public Evento(String nome, String descricao, int valor, int qntVagas, int idEvento) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.qntVagas = qntVagas;
+        this.idEvento = idEvento;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public int getQntVagas() {
+        return qntVagas;
+    }
+
+    public int getIdEvento() {
+        return idEvento;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public void setQntVagas(int qntVagas) {
+        this.qntVagas = qntVagas;
+    }
+
+    public void setIdEvento(int idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+               "nome=\'" + nome + '\'' +
+               ", descricao=\'" + descricao + '\'' +
+               ", valor=" + valor +
+               ", qntVagas=" + qntVagas +
+               ", idEvento=" + idEvento +
+               '}' + '\n';
+    }
 }
