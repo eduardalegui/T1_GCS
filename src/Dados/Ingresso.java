e895eb5dae2fcb7455fd7c996fc73032f7145426
Eduardo Hoffmann Araújo <eduardo.hoffmann1010@gmail.com>
2025-09-30 23:51:58 -0300

    Update Ingresso.java

--- src/Dados/Ingresso.java
+++ src/Dados/Ingresso.java
@@ -6,4 +6,50 @@ public class Ingresso {
     private String codigo;
     private int idIngresso;
     
+    public Ingresso(Evento evento, boolean especial, String codigo, int idIngresso) {
+        this.evento = evento;
+        this.especial = especial;
+        this.codigo = codigo;
+        this.idIngresso = idIngresso;
+    }
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public boolean isEspecial() {
+        return especial;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getIdIngresso() {
+        return idIngresso;
+    }
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+
+    public void setEspecial(boolean especial) {
+        this.especial = especial;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setIdIngresso(int idIngresso) {
+        this.idIngresso = idIngresso;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingresso{" +
+               "eventoId=" + (evento != null ? evento.getIdEvento() : "N/A") +
+               ", especial=" + especial +
+               ", codigo=\'" + codigo + "\'" +
+               ", idIngresso=" + idIngresso +
+               '\n' + "}";
+    }
 }
