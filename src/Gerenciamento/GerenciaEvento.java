d25241a3dc727a1c59032a8eaae15be73f901787
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-10-02 17:58:51 -0300

    Adição de comentários no código

--- src/Gerenciamento/GerenciaEvento.java
+++ src/Gerenciamento/GerenciaEvento.java
@@ -14,37 +14,37 @@ public class GerenciaEvento {
     }
 
     public boolean cadastrarEvento(Evento evento){
-        if (!validarParametros(evento)) {
+        if (!validarParametros(evento)) { //checagem do parametro recebido conforme a especificação
             return false;
         }
         if (hojeOuAnterior(evento)) {
             return false;
         }
 
-        eventos.add(evento);
+        eventos.add(evento); //adiciona no array
         atribuirCodigoUnico(evento);
 
         return true;
     }
 
     public void atribuirCodigoUnico(Evento evento){
-        UUID id = UUID.randomUUID();
+        UUID id = UUID.randomUUID();  //uso de uuid para geração de um id automaticamente
         String uuidSemHifen = id.toString().replace("-","");
-        String idCurto = uuidSemHifen.substring(uuidSemHifen.length()- 5);
+        String idCurto = uuidSemHifen.substring(uuidSemHifen.length()- 5); //guardar apenas os 5 ultimos caracteres por conveniencia
         evento.setIdEvento(idCurto);
     }
 
     public boolean hojeOuAnterior(Evento evento) {
-        LocalDate dataEvento = evento.getData();
-        LocalDate hoje = LocalDate.now();
+        LocalDate dataEvento = evento.getData(); //uso de LocalDate para usar seus metodos na checagem
+        LocalDate hoje = LocalDate.now(); 
 
-        return dataEvento.isBefore(hoje) || dataEvento.isEqual(hoje);
+        return dataEvento.isBefore(hoje) || dataEvento.isEqual(hoje); //retorna true se for no passado ou hoje
     }
 
-    public boolean validarParametros(Evento evento){
+    public boolean validarParametros(Evento evento){ //checa a existencia ou coerência dos paramentros recebidos
         if (evento.getNome() == null || evento.getDescricao() == null || evento.getQntIngresso() <= 0 || evento.getValor() < 0 || evento.getData() == null) {
             return false;
         }
-        return true;
+        return true;//se estiver tudo certo
     }
 }
