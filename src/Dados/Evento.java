48b2850a95640fd28fc9573a00f3cf8640f2f4bb
Matheus Guerra <m.guerra@agromai.com.br>
2025-10-02 12:47:05 -0300

    feat: cria novo método emitirIngresso() e atributos para manuseio de ingressos.

--- src/Dados/Evento.java
+++ src/Dados/Evento.java
@@ -1,6 +1,7 @@
 package Dados;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 import Gerenciamento.GerenciaIngresso;
 
@@ -12,6 +13,9 @@ public class Evento {
     private String idEvento;
     private LocalDate data;
     private GerenciaIngresso gerenciaIngresso;
+    private int qntIngressosComuns;
+    private int qntIngressosEspeciais;
+
 
     public Evento(String nome, String descricao, int valor, int qntIngresso, String idEvento, LocalDate data) {
         this.nome = nome;
@@ -21,6 +25,8 @@ public class Evento {
         this.idEvento = idEvento;
         this.data = data;
         gerenciaIngresso = new GerenciaIngresso();
+        this.qntIngressosComuns = (int) Math.floor(qntIngresso * 0.75);
+        this.qntIngressosEspeciais = (int) Math.floor(qntIngresso * 0.25);
     }
 
     // Getters
@@ -71,6 +77,61 @@ public class Evento {
 
     public void setData(LocalDate data){
         this.data = data;
+    }
+
+    public void emitirIngresso() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do participante:");
+        String nome = scanner.nextLine();
+        System.out.println("Digite o CPF do participante:");
+        String cpf = scanner.nextLine();
+        System.out.println("Digite a idade do participante:");
+        int idade = Integer.parseInt(scanner.nextLine());
+        System.out.println("O ingresso é especial? (s/n):");
+        String especialInput = scanner.nextLine();
+        boolean especial = especialInput.equalsIgnoreCase("s");
+
+        boolean cpfJaUtilizado = false;
+
+        for (Ingresso ingressoComum : gerenciaIngresso.getIngressosComuns()) {
+            if (ingressoComum.getParticipante().getCpf().equals(cpf)) {
+                cpfJaUtilizado = true;
+                break;
+            }
+        }
+
+        for (Ingresso ingressoEspecial : gerenciaIngresso.getIngressosEspeciais()) {
+            if (ingressoEspecial.getParticipante().getCpf().equals(cpf)) {
+                cpfJaUtilizado = true;
+                break;
+            }
+        }
+
+        if (cpfJaUtilizado){
+            throw new Exception("CPF já utilizado em ingressos para este evento!");
+        }
+
+        Participante participante = new Participante(null, nome, cpf, idade);
+
+        if (especial){
+            if (gerenciaIngresso.getIngressosEspeciais().size() < qntIngressosEspeciais){
+                Ingresso ingresso = new Ingresso(this, true, gerenciaIngresso.getIngressosEspeciais().size() + 1, participante);
+                gerenciaIngresso.getIngressosEspeciais().add(ingresso);
+                participante.setIngresso(ingresso);
+                System.out.println("Ingresso especial emitido com sucesso! Código: " + ingresso.getCodigo());
+            } else {
+                throw new Exception("Não há mais ingressos especiais disponíveis para este evento.");
+            }
+        } else {
+            if (gerenciaIngresso.getIngressosComuns().size() < qntIngressosComuns){
+                Ingresso ingresso = new Ingresso(this, false, gerenciaIngresso.getIngressosComuns().size() + 1, participante);
+                gerenciaIngresso.getIngressosComuns().add(ingresso);
+                participante.setIngresso(ingresso);
+                System.out.println("Ingresso comum emitido com sucesso! Código: " + ingresso.getCodigo());
+            } else {
+                throw new Exception("Não há mais ingressos comuns disponíveis para este evento.");
+            }
+        }
     }
 
     @Override
