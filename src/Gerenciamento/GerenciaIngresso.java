e8b1083763b851a0569ac073f1091ed9c38703a2
Matheus Guerra <m.guerra@agromai.com.br>
2025-10-02 12:45:19 -0300

    refactor: ajustes no gerenciador de ingressos para facilitar o manuseio e cálculo dos ingressos C e E

--- src/Gerenciamento/GerenciaIngresso.java
+++ src/Gerenciamento/GerenciaIngresso.java
@@ -1,11 +1,21 @@
 package Gerenciamento;
 import Dados.Ingresso;
 import java.util.ArrayList;
+import java.util.List;
 
 public class GerenciaIngresso {
-    private ArrayList<Ingresso> gerenciaIngresso;
+    private ArrayList<Ingresso> ingressosComuns;
+    private ArrayList<Ingresso> ingressosEspeciais;
 
-    public GerenciaIngresso (){
-        gerenciaIngresso = new ArrayList<>();
+    public GerenciaIngresso() {
+        this.ingressosComuns = new ArrayList<Ingresso>();
+        this.ingressosEspeciais = new ArrayList<Ingresso>();
+    }
+
+    public List<Ingresso> getIngressosComuns() {
+        return ingressosComuns;
+    }
+    public List<Ingresso> getIngressosEspeciais() {
+        return ingressosEspeciais;
     }
 }
