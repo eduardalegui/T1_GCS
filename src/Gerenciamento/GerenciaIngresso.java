22e689a3e20ec35075f083b2864ae26fc3be0cb1
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-10-02 19:04:31 -0300

    Adição de comentários em métodos da classe GerenciaIngresso.

--- src/Gerenciamento/GerenciaIngresso.java
+++ src/Gerenciamento/GerenciaIngresso.java
@@ -4,55 +4,55 @@ import Dados.Participante;
 import java.util.ArrayList;
 
 public class GerenciaIngresso {
-    private ArrayList<Ingresso> gerenciaIngresso;
-    private ArrayList<Participante> participantesComIngresso;
-    private ArrayList<Participante> participantesPresentes;  
+    private ArrayList<Ingresso> gerenciaIngresso; 
+    private ArrayList<Participante> participantesComIngresso; // Criação do ArrayList de participantes com ingresso
+    private ArrayList<Participante> participantesPresentes;  // Criação do ArrayList de participantes presentes
 
     public GerenciaIngresso (){
-        gerenciaIngresso = new ArrayList<>();
-        participantesComIngresso = new ArrayList<>();
-        participantesPresentes = new ArrayList<>();  
+        gerenciaIngresso = new ArrayList<>(); 
+        participantesComIngresso = new ArrayList<>(); // Inicialização do ArrayList de participantes com ingresso
+        participantesPresentes = new ArrayList<>(); // Inicialização do ArrayList de participantes presentes
     }
 
     
     public void registrarEntrada(Participante p) {
-        adicionaParticipantesComIngresso();
-        if(p.getIngresso() != null && participantesComIngresso.contains(p)) {
-            for(int i = 0; i < participantesPresentes.size(); i++) {
-                if(participantesPresentes.get(i).getIngresso().getIdIngresso() == p.getIngresso().getIdIngresso()) {
-                    System.out.println("Esse ingresso não é mais válido");
-                    return;
+        adicionaParticipantesComIngresso(); // Chama o método para preencher o ArrayList de participantes com ingresso
+        if(p.getIngresso() != null && participantesComIngresso.contains(p)) { // Verifica se o ingresso não é vazio e se ele contém o participante passado por parâmetro 
+            for(int i = 0; i < participantesPresentes.size(); i++) { // For para percorrer o ArrayList de participantes presentes
+                if(participantesPresentes.get(i).getIngresso().getIdIngresso() == p.getIngresso().getIdIngresso()) { // Verifica se o participante já apresenteu esse ingresso anteriormente
+                    System.out.println("Esse ingresso não é mais válido"); // Mensagem para dizer que a pessoa já mostrou o ingresso
+                    return; // Sai do método
                 }
             }
-            System.out.println("Entrada registrada");
-            participantesPresentes.add(p); 
+            System.out.println("Entrada registrada"); // Mensagem padrão para quando a pessoa é registrada no evento
+            participantesPresentes.add(p); // Adiciona o participante no ArrayList de participantes presentes
         }
     }
 
-    public ArrayList<Participante> getAusentes() {
-        adicionaParticipantesComIngresso();
-        ArrayList<Participante> ausentes = new ArrayList<>(); 
-        boolean ausente = true;
-        for(int i = 0; i < participantesComIngresso.size(); i++) {
-            for(int j = 0; j < participantesPresentes.size(); j++) {
-                if(participantesComIngresso.get(i).getCpf() == participantesPresentes.get(j).getCpf()) {
-                    ausente = false;
-                    break;
+    public ArrayList<Participante> getAusentes() { // Método que retorna o ArrayList de participantes ausentes
+        adicionaParticipantesComIngresso(); // Chama o método para preencher o ArrayList de participantes com ingresso
+        ArrayList<Participante> ausentes = new ArrayList<>(); // Cria e inicializa o ArrayList de participantes ausentes
+        boolean ausente = true; // Variável booleana para verificar se o participante 
+        for(int i = 0; i < participantesComIngresso.size(); i++) { // Percorre o ArrayList de participantes com ingresso
+            for(int j = 0; j < participantesPresentes.size(); j++) { // Percorre o ArrayList de participantes presentes
+                if(participantesComIngresso.get(i).getCpf() == participantesPresentes.get(j).getCpf()) { // Verifica se o participante está na lista de presentes
+                    ausente = false; // O participante está na lista de presentes
+                    break; // Sai do for atual e volta para o anterior
                 }
             }
-            if(ausente) {
-                ausentes.add(participantesComIngresso.get(i));
+            if(ausente) { // Verifica se participante é ausente
+                ausentes.add(participantesComIngresso.get(i)); // Adiciona o participante na lista de ausentes
             }
-            ausente = true;
+            ausente = true; // Reinicializa a variável booleana
         }
-        return ausentes;
+        return ausentes; // Retorna o ArrayList de ausentes
     }
 
-    public void adicionaParticipantesComIngresso(){
-        if(gerenciaIngresso.size() != participantesComIngresso.size()){
-            for(int i = 0; i<gerenciaIngresso.size(); i++){
-                Participante p = gerenciaIngresso.get(i).getParticipante();
-                participantesComIngresso.add(p);
+    public void adicionaParticipantesComIngresso(){ // Método para adicionar todos os participantes na lista de participantes com ingresso
+        if(gerenciaIngresso.size() != participantesComIngresso.size()){ // Verifica se o tamanho dos dois Arrays é diferente
+            for(int i = 0; i<gerenciaIngresso.size(); i++) { // Percorre o ArrayList de Ingressos
+                Participante p = gerenciaIngresso.get(i).getParticipante(); // Pega o participante da lista de ingresso
+                participantesComIngresso.add(p); // Adiciona o participante a lista de participantes com ingresso
             }
         }
     }
