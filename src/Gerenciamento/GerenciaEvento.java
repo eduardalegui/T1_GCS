5ee3e9cd251c6ce1c45ff07acc48c6a35fc052cb
eduardalegui <eduarda.leguisamo@edu.pucrs.br>
2025-10-02 19:46:11 -0300

    Revert "criação do método procurar evento"

--- src/Gerenciamento/GerenciaEvento.java
+++ src/Gerenciamento/GerenciaEvento.java
@@ -13,25 +13,6 @@ public class GerenciaEvento {
         eventos = new ArrayList<>();
     }
 
-    public void procurarEvento(String nome) {
-        boolean encontrado = false;
-
-        for (Evento e : eventos) {
-            if (e.getNome().toLowerCase().contains(nome.toLowerCase())) {
-                System.out.println("Evento encontrado:");
-                System.out.println("ID: " + e.getIdEvento());
-                System.out.println("Nome: " + e.getNome());
-                System.out.println("Descrição: " + e.getDescricao());
-                System.out.println("Data: " + e.getData());
-                System.out.println("Valor: " + e.getValor());
-                System.out.println("Quantidade de ingressos: " + e.getQntIngresso());
-                encontrado = true;
-            }
-        }
-
-        if (!encontrado) {
-            System.out.println("Nenhum evento encontrado com o nome informado.");
-        }
     public boolean cadastrarEvento(Evento evento){
         if (!validarParametros(evento)) { //checagem do parametro recebido conforme a especificação
             return false;
