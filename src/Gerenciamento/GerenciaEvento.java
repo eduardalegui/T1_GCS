6e6bbf3af10f8bdf46f9575fe0594da6ab6e6ddc
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-10-02 02:12:31 -0300

    Método para atribuir código único

--- src/Gerenciamento/GerenciaEvento.java
+++ src/Gerenciamento/GerenciaEvento.java
@@ -1,6 +1,7 @@
 package Gerenciamento;
 import Dados.Evento;
 import java.util.ArrayList;
+import java.util.UUID;
 
 
 public class GerenciaEvento {
@@ -8,5 +9,16 @@ public class GerenciaEvento {
 
     public GerenciaEvento(){
         eventos = new ArrayList<>();
+    }
+
+    public void cadastrarEvento(Evento evento){
+        
+    }
+
+    public void atribuirCodigoUnico(Evento evento){
+        UUID id = UUID.randomUUID();
+        String uuidSemHifen = id.toString().replace("-","");
+        String idCurto = uuidSemHifen.substring(uuidSemHifen.length()- 5);
+        evento.setIdEvento(idCurto);
     }
 }
