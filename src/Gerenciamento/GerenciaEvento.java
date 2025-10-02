23b6bfee4a6f86cd57dfa53a520787e5e4f2827e
Ana Luiza Vieira <ana.jesus1204@edu.pucrs.br>
2025-10-02 17:26:09 -0300

    criação do método procurar evento

--- src/Gerenciamento/GerenciaEvento.java
+++ src/Gerenciamento/GerenciaEvento.java
@@ -9,4 +9,25 @@ public class GerenciaEvento {
     public GerenciaEvento(){
         eventos = new ArrayList<>();
     }
+
+    public void procurarEvento(String nome) {
+        boolean encontrado = false;
+
+        for (Evento e : eventos) {
+            if (e.getNome().toLowerCase().contains(nome.toLowerCase())) {
+                System.out.println("Evento encontrado:");
+                System.out.println("ID: " + e.getIdEvento());
+                System.out.println("Nome: " + e.getNome());
+                System.out.println("Descrição: " + e.getDescricao());
+                System.out.println("Data: " + e.getData());
+                System.out.println("Valor: " + e.getValor());
+                System.out.println("Quantidade de ingressos: " + e.getQntIngresso());
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("Nenhum evento encontrado com o nome informado.");
+        }
+    }
 }
