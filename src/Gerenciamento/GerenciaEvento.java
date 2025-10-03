1cc047214d014d8823c09df45620582c87a97ebd
Arthur <arthurtomazelli22@gmail.com>
2025-10-03 00:17:31 -0300

    Implementar método para cancelar evento

--- src/Gerenciamento/GerenciaEvento.java
+++ src/Gerenciamento/GerenciaEvento.java
@@ -1,22 +1,38 @@
 package Gerenciamento;
-import Dados.Evento;
-import java.util.ArrayList;
 
+import Dados.Evento;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class GerenciaEvento {
     private ArrayList<Evento> eventos;
 
-    public GerenciaEvento(){
+    public GerenciaEvento() {
         eventos = new ArrayList<>();
     }
 
-    public boolean cancelarEvento(String nome){
-        Evento eventoRemover = procurarEventoEspecifico(nome);
-        
-        if(eventoRemover == null){
+    //#8 feature adicional - Cancelar evento com pelo menos 15 dias de antecedência.
+    public boolean cancelarEvento(String nome) {
+        //chamar o método listarEventos antes de chamar esse.
+
+        //o método que é chamado abaixo vai ser implementado na branch da issue #3 (Procurar evento específico)
+        Evento eventoRemover = encontrarEvento(nome);
+
+        if (eventoRemover == null) {
             return false;
-        } else{
-            eventos.remove(eventoRemover);
+        } else {
+            LocalDate hoje = LocalDate.now();
+            LocalDate dataEvento = eventoRemover.getData();
+
+            if (dataEvento.isAfter(hoje.plusDays(14))) {
+                eventos.remove(eventoRemover);
+                //fazer no app (caso true): System.out.println("Evento cancelado com sucesso.");
+                return true;
+            } else {
+                //fazer no app (caso false): System.out.println("Não foi possível realizar o cancelamento. O evento está ocorrerá em menos de 15 dias.");
+                return false;
+            }
         }
     }
 }
