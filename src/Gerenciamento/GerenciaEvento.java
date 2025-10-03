17f44350700c265c83800e910952f29c4b9a8358
Ana Luiza Vieira <ana.jesus1204@edu.pucrs.br>
2025-10-02 23:52:50 -0300

    Adesão do método "encontraEvento".

--- src/Gerenciamento/GerenciaEvento.java
+++ src/Gerenciamento/GerenciaEvento.java
@@ -1,33 +1,41 @@
 package Gerenciamento;
+
 import Dados.Evento;
 import java.util.ArrayList;
-
 
 public class GerenciaEvento {
     private ArrayList<Evento> eventos;
 
-    public GerenciaEvento(){
+    public GerenciaEvento() {
         eventos = new ArrayList<>();
     }
 
-    public void procurarEvento(String nome) {
-        boolean encontrado = false;
+    // Método para procurar um evento pelo nome e retornar todos atributos dele
 
+    public void procurarEvento(String nome) {
+        Evento e = encontrarEvento(nome);
+        if (e != null) {
+            System.out.println("Evento encontrado:");
+            System.out.println("ID: " + e.getIdEvento());
+            System.out.println("Nome: " + e.getNome());
+            System.out.println("Descrição: " + e.getDescricao());
+            System.out.println("Data: " + e.getData());
+            System.out.println("Valor: " + e.getValor());
+            System.out.println("Quantidade de ingressos: " + e.getQntIngresso());
+        } else {
+            System.out.println("Nenhum evento encontrado com o nome informado.");
+        }
+    }
+
+    // Método para encontrar o evento e retornar ele como objeto
+
+    public Evento encontrarEvento(String nome) {
         for (Evento e : eventos) {
             if (e.getNome().toLowerCase().contains(nome.toLowerCase())) {
-                System.out.println("Evento encontrado:");
-                System.out.println("ID: " + e.getIdEvento());
-                System.out.println("Nome: " + e.getNome());
-                System.out.println("Descrição: " + e.getDescricao());
-                System.out.println("Data: " + e.getData());
-                System.out.println("Valor: " + e.getValor());
-                System.out.println("Quantidade de ingressos: " + e.getQntIngresso());
-                encontrado = true;
+                return e;
             }
         }
 
-        if (!encontrado) {
-            System.out.println("Nenhum evento encontrado com o nome informado.");
-        }
+        return null;
     }
 }
