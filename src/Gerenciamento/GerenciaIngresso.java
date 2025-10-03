466e6d84fe318f3f9d1c1f9a9a57e8f22838e373
Isadora <isadoraspode1@gmail.com>
2025-10-03 09:48:10 -0300

    Correção duplicada

--- src/Gerenciamento/GerenciaIngresso.java
+++ src/Gerenciamento/GerenciaIngresso.java
@@ -16,54 +16,6 @@ public class GerenciaIngresso {
         this.random = new Random();
     }
 
-        public Ingresso emitirIngresso (Evento evento, Participante participante, boolean isEspecial) {
-
-            String codigo = gerarCodigoIngresso(evento);
-            int idIngresso = gerarIdIngresso();
-
-            Ingresso novoIngresso = new Ingresso(evento, isEspecial, codigo, idIngresso, participante);
-            this.ingressosVendidos.add(novoIngresso);
-
-            if (participante.getIngresso() == null) {
-                participante.setIngresso(novoIngresso);
-            }
-
-            System.out.println("SUCESSO - Ingresso " + codigo + " emitido para " + participante.getNome());
-            return novoIngresso;
-        }
-
-    public int getTotalIngressosVendidos () {
-        return ingressosVendidos.size();
-    }
-
-    public int getIngressosVendidosNormais () {
-        int count = 0;
-        for (Ingresso ingresso : ingressosVendidos) {
-            if (!ingresso.isEspecial()) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public int getIngressosVendidosEspeciais () {
-        int count = 0;
-        for (Ingresso ingresso : ingressosVendidos) {
-            if (ingresso.isEspecial()) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private String gerarCodigoIngresso (Evento evento){
-        return evento.getIdEvento() + "-" + (ingressosVendidos.size() + 1) + new Random().nextInt(100);
-    }
-
-    private int gerarIdIngresso () {
-        return ingressosVendidos.size() + 1;
-    }
-
 
     public Ingresso emitirIngresso(Evento evento, Participante participante, boolean isEspecial) {
 
