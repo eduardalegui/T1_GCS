1b35bce7096fd2dd42507fb0cdd430eec47d3179
Isadora <isadoraspode1@gmail.com>
2025-10-03 00:26:49 -0300

    Adiciona um evento à lista de eventos gerenciados.

--- src/Gerenciamento/GerenciaEvento.java
+++ src/Gerenciamento/GerenciaEvento.java
@@ -1,12 +1,24 @@
 package Gerenciamento;
 import Dados.Evento;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.time.LocalDate;
 
 
 public class GerenciaEvento {
     private ArrayList<Evento> eventos;
+    private int proximoIdEvento = 1;
 
     public GerenciaEvento(){
         eventos = new ArrayList<>();
     }
+
+    public Evento adicionarEvento(String nome, String descricao, int valor, int qntIngresso, LocalDate data) {
+        String idGerado = "EVE" + String.format("%03d", proximoIdEvento++);
+        Evento novoEvento = new Evento(nome, descricao, valor, qntIngresso, idGerado, data);
+        eventos.add(novoEvento);
+        return novoEvento;
+    }
+
+
 }
