d772b7e1202e6424caf463573b12edd19022228f
Isadora <isadoraspode1@gmail.com>
2025-10-03 01:42:31 -0300

    Reapply " Gera um código único e um ID"
    This reverts commit 7ee3e2127aed9ca4061ce7e4464141021db71648.

--- src/Gerenciamento/GerenciaIngresso.java
+++ src/Gerenciamento/GerenciaIngresso.java
@@ -1,11 +1,26 @@
 package Gerenciamento;
 import Dados.Ingresso;
 import java.util.ArrayList;
+import Dados.Evento;
+import Dados.Participante;
+import java.util.List;
+import java.util.Random;
 
 public class GerenciaIngresso {
-    private ArrayList<Ingresso> gerenciaIngresso;
+    private ArrayList<Ingresso> ingressosVendidos;
+    private Random random;
 
-    public GerenciaIngresso (){
-        gerenciaIngresso = new ArrayList<>();
+
+    public GerenciaIngresso() {
+        this.ingressosVendidos = new ArrayList<>();
+        this.random = new Random();
     }
-}
+
+        public Ingresso emitirIngresso (Evento evento, Participante participante, boolean isEspecial){
+        
+            String codigo = gerarCodigoIngresso(evento);
+            int idIngresso = gerarIdIngresso();
+
+            Ingresso novoIngresso = new Ingresso(evento, isEspecial, codigo, idIngresso, participante);
+            this.ingressosVendidos.add(novoIngresso);
+
