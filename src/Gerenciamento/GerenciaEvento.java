1234c52b009eeeef9b7d4b6e8c5b042b3b435c7e
Eduardo Paz <edu.epaz@gmail.com>
2025-10-03 21:40:16 -0300

    feat: implementação do método "calcularIngressoEspecial" e "consultarDetalhesEvento"

--- src/Gerenciamento/GerenciaEvento.java
+++ src/Gerenciamento/GerenciaEvento.java
@@ -9,15 +9,30 @@ public class GerenciaEvento {
 
     public GerenciaEvento() {
         eventos = new ArrayList<>();
-        eventos.add(new Evento(
-            "Show do Coldplay",
-            "Concerto internacional",
-            250,
-            1000,
-            "EVT001",
-            LocalDate.of(2025, 11, 10),
-            "Estádio Beira-Rio"
-        ));
+    }
+
+    public int calcularIngressoEspecial(Evento evento){ //15% dos ingressos são especiais
+        int total = evento.getQntIngresso(); 
+        int especial = (int) (total * 0.15); //casting para int, pois o resultado é double
+        return especial;
+    }
+
+    public Evento consultarDetalhesEvento(String nome){
+        Evento escolhido = encontrarEvento(nome); //o método "encontraEvento" foi feito por outra pessoa
+        if (escolhido == null) { //se o evento não for encontrado
+            System.out.println("Evento não encontrado.");
+        }
+        System.out.println("Nome: " + escolhido.getNome());
+        System.out.println("Descrição: " + escolhido.getDescricao());
+        System.out.println("Valor: " + escolhido.getValor());
+        System.out.println("Numero de ingressos: " + (escolhido.getQntIngresso() - calcularIngressoEspecial(escolhido)));
+        System.out.println("Numero de ingressos especiais: " + calcularIngressoEspecial(escolhido));
+        System.out.println("Data: " + escolhido.getData());
+        /*
+        A opção para emissão de um ingresso fica disponível após o usuário consultar os detalhes,
+        mas não ocorre automaticamente
+        */
+        return escolhido;
     }
 
     public List<Evento> getEventos() {
